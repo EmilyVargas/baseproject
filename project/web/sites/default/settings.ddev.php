d96fe5ff40dfb23087a1838da525613adf7c4c5b
Emily Vargas <Emily.Vargas@live.com>
2023-04-22 20:37:59 +0000

    Repaso 22/4/2023

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'MlUnBOZEqnqXJDPypPaTjBHAoQPOoSwQIGeapQeuKbHPVYsizNSvmsVXUicoFpGq';
+$settings['hash_salt'] = 'hGiKGvjqrbcDdJFJGzfENNLZjqMZdEhmiHHLSHHaMMCYqtMLYZuIFSElOHpoYWWk';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
